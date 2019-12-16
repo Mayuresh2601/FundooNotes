4cf26f99a0fd6c1e6dd16fdd7be0b170f99a7ba1
Mayuresh2601 <mssonar26@gmail.com>
2019-12-16 14:45:55 +0530

    Integrated Swagger

--- src/main/java/com/bridgelabz/fundoonotes/user/config/Config.java
+++ src/main/java/com/bridgelabz/fundoonotes/user/config/Config.java
@@ -1,7 +1,7 @@
 /******************************************************************************
 *  
 *  Purpose: To Implement Fundoo Notes App
-*  @class To Encrypt the Password
+*  @class To Configure Maven Project
 *  @author  Mayuresh Sunil Sonar
 *
 ******************************************************************************/
@@ -12,7 +12,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 public class Config {
 	
 	
@@ -33,5 +40,15 @@ public class Config {
 	public ModelMapper mapper() {
 		
 		return new ModelMapper();
+	}
+
+	
+	/**Method: To Implement Swagger On UserInterface
+	 * @return SwaggerUI
+	 */
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
 	}
 }
